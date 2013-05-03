3391d17d9eb8d13471b297c988ea11e73a470de4
David Bernard <david.bernard.31@gmail.com>
2013-05-03 11:28:34 +0200

    add support for common Transform component and simple_audio

---
+++ lib/simple_audio.dart
@@ -0,0 +1,139 @@
+library simple_audio;
+
+import 'package:dartemis/dartemis.dart';
+import 'package:dartemis_addons/utils.dart';
+import 'package:dartemis_addons/transform.dart';
+import 'package:simple_audio/simple_audio.dart';
+
+/**
+ * Component use to store a bag of name of [AudioClip] to play.
+ * When the [System_Audio] start to play, name is removed from the bag.
+ * [System_Audio] start to play ASAP.
+ */
+class AudioDef extends ComponentPoolable {
+  final l = new LinkedBag<String>();
+
+  AudioDef._();
+  static _ctor() => new AudioDef._();
+  factory AudioDef() {
+    var c = new Poolable.of(AudioDef, _ctor);
+    c.cleanUp();
+    return c;
+  }
+
+  void cleanUp() {
+    l.clear();
+  }
+
+  /// this is a sugar method for [l].add([a])
+  /// sugar because you can write
+  ///
+  ///    new AudioDef()
+  ///      ..add("boost")
+  ///      ..add("alarm")
+  ///
+  AudioDef add(String a) {
+    l.add(a);
+    return this;
+  }
+}
+
+class _AudioCache extends Component {
+  final AudioSource source;
+
+  _AudioCache(this.source);
+}
+/// ClipProvider find an AudioClip from name;
+/// eg a clipProvider from assetPackManager :
+///
+///     (x) => _assetManager[x]
+///
+typedef AudioClip ClipProvider(String name);
+
+class System_Audio extends EntityProcessingSystem {
+  ComponentMapper<Transform> _transformMapper;
+  ComponentMapper<AudioDef> _objDefMapper;
+  ComponentMapper<_AudioCache> _objCacheMapper;
+  GroupManager _groupManager;
+
+  var _listener;
+  var _positional = false;
+  final AudioManager _audioManager;
+
+  /// the function to find AudioClip from name,
+  /// default implementation is [clipProvider0]
+  final ClipProvider clipProvider;
+
+  System_Audio(this._audioManager, {clipProvider : clipProvider0, positional : false}):super(Aspect.getAspectForAllOf([AudioDef])), _positional = positional;
+
+  void initialize(){
+    _transformMapper = new ComponentMapper<Transform>(Transform, world);
+    _objDefMapper = new ComponentMapper<AudioDef>(AudioDef, world);
+    _objCacheMapper = new ComponentMapper<_AudioCache>(_AudioCache, world);
+    _groupManager = world.getManager(GroupManager) as GroupManager;
+  }
+
+  /// a default implementation for clipProvider, that retreive clip from [AudioManager]
+  ///   (x) = > _audioManager.findClip(x);
+  AudioClip clipProvider0(x) => _audioManager.findClip(x);
+
+  void processEntity(entity) {
+    var cache = _objCacheMapper.getSafe(entity);
+    if (cache != null) {
+      var obj = cache.source;
+      _applyTransform(obj, entity);
+      var def = _objDefMapper.get(entity);
+      def.l.iterateAndUpdate((x) {
+        var clip = clipProvider(x);
+        if (clip == null) {
+          //TODO log("can't play sound '${x}' : notfound in audioManager nor assetManager");
+        } else {
+          //TODO log("play ${x} : ${_assetManager.getAssetAtPath(x).url} : ${clip}");
+          if (obj != _listener) {
+            obj.playOnce(clip);
+          } else {
+            _audioManager.music.stop();
+            _audioManager.music.clip = clip;
+            _audioManager.music.play(loop : false);
+          }
+        }
+        return null;
+      });
+    }
+  }
+
+  void inserted(Entity entity){
+    var objDef = _objDefMapper.get(entity);
+    var obj = _audioManager.makeSource(entity.id.toString());
+    entity.addComponent(new _AudioCache(obj));
+    entity.changedInWorld();
+    _applyTransform(obj, entity);
+    if (_groupManager.isInGroup(entity, GROUP_AUDIOLISTENER)) {
+      //log("set audiolistener");
+      _listener = obj;
+    }
+  }
+
+  void removed(Entity entity){
+    var cache = _objCacheMapper.getSafe(entity);
+    if (cache != null) {
+      cache.source.stop();
+      if (cache.source == _listener) _listener = null;
+    }
+    entity.removeComponent(_AudioCache);
+    //log("removed audio ${entity}");
+  }
+
+  void _applyTransform(obj, Entity entity) {
+    if (! _positional) return;
+    var tf = _transformMapper.getSafe(entity);
+    if (obj != null && tf != null) {
+      obj.positional = true;
+      obj.setPosition(tf.position3d.x, tf.position3d.y, tf.position3d.z);
+      if (obj == _listener) {
+        _audioManager.setPosition(tf.position3d.x, tf.position3d.y, tf.position3d.z);
+      }
+    }
+  }
+
+}
---
+++ lib/transform.dart
@@ -0,0 +1,54 @@
+library transform;
+
+import 'package:dartemis/dartemis.dart';
+import 'package:vector_math/vector_math.dart';
+
+class Transform extends ComponentPoolable {
+  vec3 position3d;
+  vec3 rotation3d;
+  vec3 scale3d;
+
+  // 2d view
+  vec2 _position2d = new vec2.zero();
+  double get angle => rotation3d.z;
+  set angle(double v) => rotation3d.z = v;
+  vec2 get position {
+    _position2d.x = position3d.x;
+    _position2d.y = position3d.y;
+    return _position2d;
+  }
+  set position(vec2 v) {
+    position3d.x = v.x;
+    position3d.y = v.y;
+  }
+
+  Transform._();
+  static _ctor() => new Transform._();
+  factory Transform.w2d(num x, num y, num a) {
+    return new Transform.w3d(new vec3(x, y, 0), new vec3(0,0,a));
+  }
+  factory Transform.w3d(vec3 position, [vec3 rotation, vec3 scale]) {
+    var c = new Poolable.of(Transform, _ctor) as Transform;
+    c.position3d = position;
+    c.rotation3d = (rotation == null) ? new vec3(0,0,0) : rotation;
+    c.scale3d = (scale == null) ? new vec3(1,1,1) : scale;
+    return c;
+  }
+  /// this method mofidy the Transform (usefull for creation)
+  /// return this
+  Transform lookAt(vec3 target, [vec3 up]) {
+    up = (up == null) ? new vec3(0.0, 1.0, 0.0) : up;
+    var m = makeViewMatrix(position3d, target, up).getRotation();
+    // code from (euler order XYZ)
+    // https://github.com/mrdoob/three.js/blob/master/src/math/Vector3.js
+    rotation3d.y = math.asin( clamp( m.row2.x, -1.0 ,1.0 ) );
+    if ( m.row2.x.abs() < 0.99999 ) {
+      rotation3d.x = math.atan2( - m.row2.y, m.row2.z );
+      rotation3d.z = math.atan2( - m.row1.x, m.row0.x );
+    } else {
+      rotation3d.x = math.atan2( m.row1.z, m.row1.y );
+      rotation3d.z = 0.0;
+    }
+    return this;
+  }
+}
