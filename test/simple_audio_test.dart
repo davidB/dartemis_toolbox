a5a507fc758cf8c57428abcfa85de9e042eef147
David Bernard <david.bernard.31@gmail.com>
2013-05-03 21:06:29 +0200

    add missing fake test for transform and simple_audio

---
+++ test/simple_audio_test.dart
@@ -0,0 +1,10 @@
+library utils_test;
+
+import 'package:unittest/unittest.dart';
+//import 'package:dartemis_addons/simple_audio.dart';
+
+main() {
+  print("simple_audio require dart:html => only run in browser");
+}
+
+
---
+++ test/transform_test.dart
@@ -0,0 +1,9 @@
+library utils_test;
+
+import 'package:unittest/unittest.dart';
+import 'package:dartemis_addons/transform.dart';
+
+main() {
+}
+
+
