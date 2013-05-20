c7f208aca4b6bfb1e2dd975adeccbcd9c9071c0e
David Bernard <david.bernard.31@gmail.com>
2013-05-20 12:42:42 +0200

    build: build demo of colors

--- build.dart
+++ build.dart
@@ -3,5 +3,5 @@ import 'package:web_ui/component_build.dart';
 
 // Ref: http://www.dartlang.org/articles/dart-web-components/tools.html
 main() {
-  build(new Options().arguments, ['web/ease_graphics.html', 'web/xtchart_demo.html', 'web/demos.html']);
+  build(new Options().arguments, ['web/ease_graphics.html', 'web/xtchart_demo.html', 'web/demos.html', 'web/colors_demo.html']);
 }
