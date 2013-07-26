290d18484d5d83ad3c4c4ff2d5fc13880757c996
David Bernard <david.bernard.31@gmail.com>
2013-07-26 14:46:37 +0200

    publish reports to gh-pages

---
+++ colors_demo.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+
+void main() {
+  // Enable this to use Shadow DOM in the browser.
+  //useShadowDom = true;
+  // xtag is null until the end of the event loop (known dart web ui issue)
+}
