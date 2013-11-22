7b5d56f8d823fa3a96c668d900d04dcedc6de6bf
David Bernard <david.bernard.31@gmail.com>
2013-11-22 13:10:07 +0100

    replace web_ui by html_toolbox for ease_graphics demo

--- web/ease_graphics.dart
+++ web/ease_graphics.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
 import 'package:web_ui/web_ui.dart';
 import 'package:dartemis_toolbox/ease.dart' as ease;
+import 'package:html_toolbox/html_toolbox.dart';
+import 'package:html_toolbox/widgets_charts.dart';
 
 @observable
 var eases = ease.all;
@@ -12,4 +14,23 @@ var eases = ease.all;
 void main() {
   // Enable this to use Shadow DOM in the browser.
   //useShadowDom = true;
+  var tmpl = new MicroTemplate(querySelector(".ease"));
+  var items = eases.keys.map((k) => {
+    "k" : k
+  }).toList();
+  var validator = new NodeValidatorBuilder.common()
+  ..allowHtml5()
+  ..allowElement('canvas', attributes : ['data-ease'])
+  ;
+  tmpl.apply(items, validator: validator);
+
+  querySelectorAll("canvas[data-ease]").forEach((Element el){
+    var fname = el.dataset["ease"];
+    var f = fname.startsWith("goback.") ? ease.goback(eases[fname.substring("goback.".length)]) : eases[fname];
+    new ChartF()
+    ..el = el
+    ..fct = f
+    ..update();
+  });
+
 }
